dcb5e95e82035ff6f521456c9450b5255f20313b
glz <glz@mail.com>
2014-04-07 21:53:12 +0800

    lesson 4 building the middleware stack

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
 var http = require("http");
 module.exports = function() {
-	var myexpress = function(req, res) {
-		res.statusCode = 404;
-		res.end();
+	var app = function(req, res, next) {
+		app.handle(req,res,next);
 	}
 
-	myexpress.listen = function(port, callback) {
-		return http.createServer(this).listen(port, callback);
+	app.stack = [];
+
+	app.handle = function(req,res,out) {
+		var index = 0,
+				stack = this.stack;
+
+		function next(err) {
+			var layer = stack[index++];
+
+			if(!layer) {
+				if(out) {
+					return out(err);
+				} else {
+					res.statusCode = err ? 500 : 404;
+					res.end();
+				}
+			} else {
+				try {
+					var arity = layer.length;
+					if (err) {
+						if (arity == 4) {
+							layer(err,req,res,next);
+						} else {
+							next(err);
+						}
+					} else if (arity < 4) {
+						layer(req,res,next);
+					} else {
+						next();
+					}
+				} catch(e) {
+					next(e);
+				}
+			}
+		}
+		next();
 	}
-	return myexpress;
+
+	app.listen = function(port,callback) {
+		return http.createServer(this).listen(port,callback);
+	}
+
+	app.use = function(midd) {
+		this.stack.push(midd);
+		return this;
+	};
+	return app;
 }
--- test/app_spec.js
+++ test/app_spec.js
@@ -1,5 +1,5 @@
 var express = require("../")
-var request = require("supertest")
+  , request = require("supertest")
 	, expect = require("chai").expect
 	, http = require("http");
 
@@ -7,8 +7,8 @@ describe("app", function() {
 	var app = express();
 	describe("create http server", function() {
 		// write your test here
-		var server = http.createServer(app);
 		it("responds to /foo with 404", function(done) {
+			var server = http.createServer(app);
 			request(server).get("/foo").expect(404).end(done);
 		});
 	});
@@ -27,5 +27,158 @@ describe("app", function() {
 		it("responds to /foo with 404", function(done) {
 			request("http://localhost:" + port).get("/foo").expect(404).end(done);
 		});
+	});
+
+	describe(".use", function() {
+		var m1 = function() {};
+		var m2 = function() {};
+		var stackSize = app.stack.length;
+		app.use(m1);
+		app.use(m2);
+
+		it("should be able to add middleware to stack", function(){
+			expect(app.stack.length).to.equal(stackSize + 2);
+			expect(app.stack[0]).to.equal(m1);
+			expect(app.stack[1]).to.equal(m2);
+		});
+	});
+});
+
+describe("calling middleware stack", function() {
+	var app;
+	beforeEach(function() {
+		app = new express();
+	});
+
+	it("should be able to call a single middleware", function(done) {
+		var m1 = function(req,res,next) {
+			res.end("hello from m1");
+		}
+		app.use(m1);
+		request(app).get("/").expect("hello from m1").end(done);
+	});
+
+	it("should be able to call next to go to the next middleware", function(done) {
+		var m1 = function(req,res,next) {
+			next();
+		};
+
+		var m2 = function(req,res,next) {
+			res.end("hello from m2");
+		};
+		app.use(m1);
+		app.use(m2);
+		request(app).get("/").expect("hello from m2").end(done);
+	});
+
+	it("should 404 at the end of middleware chain", function(done) {
+		var m1 = function(req,res,next) {
+			next();
+		};
+		var m2 = function(req,res,next) {
+			next();
+		};
+		app.use(m1);
+		app.use(m2);
+		request(app).get("/").expect(404).end(done);
+	});
+
+	it("should 404 if no middleware is added", function(done) {
+		request(app).get("/").expect(404).end(done);
+	});
+});
+
+describe("Implement Error Handling", function() {
+	var app;
+	beforeEach(function() {
+		app = express();
+	});
+
+	it("should return 500 for unhandled error", function(done) {
+		var m1 = function(req,res,next) {
+			next(new Error("boom!"));
+		}
+		app.use(m1);
+
+		request(app).get("/").expect(500).end(done);
+	});
+
+	it("should return 500 for uncaught error", function(done) {
+		var m1 = function(req,res,next) {
+			throw new Error("boom!");
+		}
+		app.use(m1);
+		request(app).get("/").expect(500).end(done);
+	});
+
+	it("should skip error handlers when next is called without an error", function(done) {
+		var m1 = function(req,res,next) {
+			next();
+		}
+
+		var e1 = function(err,req,res,next) {
+			// timeout
+		}
+
+		var m2 = function(req,res,next) {
+			res.end("m2");
+		}
+		app.use(m1);
+		app.use(e1);
+		app.use(m2);
+		request(app).get("/").expect("m2").end(done);
+	});
+
+	it("should skip normal middlewares if next is called with an error", function(done) {
+		var m1 = function(req,res,next) {
+			next(new Error("boom!"));
+		}
+
+		var m2 = function(req,res,next) {
+			// timeout
+		}
+
+		var e1 = function(err,req,res,next) {
+			res.end("e1");
+		}
+
+		app.use(m1);
+		app.use(m2);
+		app.use(e1);
+		request(app).get("/").expect("e1").end(done);
+	});
+});
+
+describe("Implement App Embedding As Middleware", function() {
+	it("should pass unhandled request to parent", function(done) {
+		app = new express();
+		subApp = new express();
+
+		function m2(req,res,next) {
+			res.end("m2");
+		}
+
+		app.use(subApp);
+		app.use(m2);
+		request(app).get("/").expect("m2").end(done);
+	});
+
+	it("should pass unhandled error to parent", function(done) {
+		app = new express();
+		subApp = new express();
+
+		function m1(req,res,next) {
+			next("m1 error");
+		}
+
+		function e1(err,req,res,next) {
+			res.end(err);
+		}
+
+		subApp.use(m1);
+
+		app.use(subApp);
+		app.use(e1);
+		request(app).get("/").expect("m1 error").end(done);
 	});
 });
